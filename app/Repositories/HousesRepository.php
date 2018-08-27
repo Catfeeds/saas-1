59175669dd6becdab56151e234e515691d03af89
郑宇 <1130367447@qq.com>
2018-08-27 01:30:32 +0000

    整理字段

--- app/Repositories/HousesRepository.php
+++ app/Repositories/HousesRepository.php
@@ -12,41 +12,49 @@ class HousesRepository extends Model
     {
         return House::create([
             'guid' => Common::getUuid(),
+            'owner_info' => $request->owner_info,//业主电话
+            'floor' => $request->floor,//所在楼层
+            'name' => $request->name,//小区
+            'house_number' => $request->house_number,//房号
+            'building_block_guid' => $request->building_block_guid,//楼座guid
+            'grade' => $request->grade,//房源等级
+            'public_private' => $request->public_private,//盘别
+            'price' => $request->price,//租金
+            'price_unit' => $request->price_unit,//租金单位
+            'payment_type' => $request->payment_type,//付款方式
+            'increasing_situation_remark' => $request->increasing_situation_remark,//递增情况
+            'cost_detail' => $request->cost_detail,//费用明细
+            'acreage' => $request->acreage,//面积
+            'split' => $request->split,//可拆分
+            'mini_acreage' => $request->mini_acreage,//最小面积
+            'total_floor' => $request->total_floor,//总楼层
+            'floor_height' => $request->floor_height,//层高
+            'register_company' => $request->register_company,//注册公司
+            'type' => $request->type,//写字楼类型
+            'orientation' => $request->orientation,//朝向
+            'renovation' => $request->renovation,//装修
+            'open_bill' => $request->open_bill,//可开发票
+            'station_number' => $request->station_number,//工位数量
+            'rent_free' => $request->rent_free,//免租期
+            'support_facilities' => $request->support_facilities,//配套
+            'source' => $request->source,//渠道来源
+            'status' => $request->status,//现状
+            'shortest_lease' => $request->shortest_lease,//最短租期
+            'remarks' => $request->remarks,//备注
+
+
+
+
+
+
+
             'house_type' => $request->house_type,
-            'public_private' => $request->public_private,
-            'owner_info' => $request->owner_info,
-            'name' => $request->name,
             'pedestal' => $request->pedestal,
             'pedestal_unit' => $request->pedestal_unit,
             'unit' => $request->unit,
             'unit_unit' => $request->unit_unit,
-            'house_number' => $request->house_number,
-            'grade' => $request->grade,
-            'price' => $request->price,
-            'price_unit' => $request->price_unit,
-            'payment_type' => $request->payment_type,
-            'increasing_situation_remark' => $request->increasing_situation_remark,
-            'cost_detail' => $request->cost_detail,
-            'acreage' => $request->acreage,
-            'split' => $request->split,
-            'mini_acreage' => $request->mini_acreage,
-            'floor' => $request->floor,
-            'total_floor' => $request->total_floor,
-            'floor_height' => $request->floor_height,
             'property_grade' => $request->property_grade,
             'property_fee' => $request->property_fee,
-            'register_company' => $request->register_company,
-            'type' => $request->type,
-            'orientation' => $request->orientation,
-            'renovation' => $request->renovation,
-            'open_bill' => $request->open_bill,
-            'station_number' => $request->station_number,
-            'rent_free' => $request->rent_free,
-            'support_facilities' => $request->support_facilities,
-            'source' => $request->source,
-            'status' => $request->status,
-            'shortest_lease' => $request->shortest_lease,
-            'remarks' => $request->remarks,
             'house_type_img' => $request->house_type_img,
             'indoor_img' => $request->indoor_img,
             'outdoor_img' => $request->outdoor_img,

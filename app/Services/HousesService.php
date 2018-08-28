44f44ad8293b8ad06b48bbca2bc5815c4a612db7
luozhen <15902773450@163.com>
2018-08-28 14:43:11 +0800

    通过楼座获取城市

--- app/Services/HousesService.php
+++ app/Services/HousesService.php
@@ -60,15 +60,15 @@ class HousesService
     }
 
     // 通过楼座获取城市
-    public function adoptBuildingBlockGetCity($BuildingBlockId)
+    public function adoptBuildingBlockGetCity($BuildingBlockGuid)
     {
-        $temp = BuildingBlock::find($BuildingBlockId);
+        $temp = BuildingBlock::find($BuildingBlockGuid);
 
         // 拼接商圈获取城市数据
-        $arr[] = $temp->building->area->city->id;
-        $arr[] = $temp->building->area->id;
-        $arr[] = $temp->building->id;
-        $arr[] = $BuildingBlockId;
+        $arr[] = $temp->building->area->city->guid;
+        $arr[] = $temp->building->area->guid;
+        $arr[] = $temp->building->guid;
+        $arr[] = $BuildingBlockGuid;
 
         return $arr;
     }

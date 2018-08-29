9af71e97593cd8172c2dac7e85c067ac12872646
luozhen <15902773450@163.com>
2018-08-29 16:34:21 +0800

    带看数据表

--- database/migrations/2018_08_29_095530_create_visits_table.php
+++ database/migrations/2018_08_29_095530_create_visits_table.php
@@ -15,12 +15,16 @@ class CreateVisitsTable extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->char('guid', 32)->primary()->comment('guid主键');
-
-
-
-
+            $table->char('visit_user',32)->nullable()->comment('带看人');
+            $table->char('accompany',32)->nullable()->comment('陪看人');
+            $table->char('rel_guid',32)->nullable()->comment('带看的客源/房源');
+            $table->string('remarks',255)->nullable()->comment('带看备注');
+            $table->string('visit_img',32)->nullable()->comment('带看单');
+            $table->dateTime('visit_date')->nullable()->comment('带看日期');
+            $table->tinyInteger('visit_time')->nullable()->comment('带看时间 1: 上午 2: 下午 3: 晚上');
             $table->timestamps();
         });
+        \DB::statement("alter table `visits` comment'带看表'");
     }
 
     /**

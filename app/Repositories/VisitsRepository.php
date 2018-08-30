1ab426fc1b8bb5dd6988d0b42b78b1568c9077d7
刘坤涛 <287667404@qq.com>
2018-08-30 06:36:21 +0000

    预加载

--- app/Repositories/VisitsRepository.php
+++ app/Repositories/VisitsRepository.php
@@ -11,7 +11,7 @@ class VisitsRepository extends Model
     //获取房源或者客源带看列表
     public function visitsList($request, $service)
     {
-        $res = Visit::where('cover_rel_guid', $request->cover_rel_guid)->latest()->paginate($request->per_page??10);
+        $res = Visit::with('user', 'accompanyUser', 'house')->where('cover_rel_guid', $request->cover_rel_guid)->latest()->paginate($request->per_page??10);
         $visit = [];
         foreach ($res as $key =>  $v) {
             $visit[$key] = $service->getData($v);

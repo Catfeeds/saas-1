f9a08fe612c0b16683b5bf2ca9fb8f879d229d1a
刘坤涛 <287667404@qq.com>
2018-09-03 03:33:11 +0000

    修改

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -15,4 +15,99 @@ class Customer extends BaseModel
         'acreage' => 'array',
         'floor' => 'array'
     ];
+
+    protected $appends = [
+        'level_cn', 'type_cn', 'renovation_cn'
+    ];
+
+    //等级中文
+    public function getLevelCnAttribute()
+    {
+        switch ($this->level) {
+            case 1:
+                return 'A';
+                break;
+            case 2:
+                return 'B';
+                break;
+            case 3:
+                return 'C';
+                break;
+                default;
+                break;
+        }
+    }
+
+    //房源类型中文
+    public function getTypeCnAttribute()
+    {
+        switch ($this->type) {
+            case 1:
+                return '纯写字楼';
+                break;
+            case 2:
+                return '商住楼';
+                break;
+            case 3:
+                return '商业综合体';
+                break;
+            case 4:
+                return '酒店写字楼';
+                break;
+            case 5:
+                return '其他';
+                break;
+                default;
+                break;
+        }
+    }
+
+    // 装修中文
+    public function getRenovationCnAttribute()
+    {
+        switch ($this->renovation) {
+            case 1:
+                return '豪华装修';
+                break;
+            case 2:
+                return '精装修';
+                break;
+            case 3:
+                return '中装修';
+                break;
+            case 4:
+                return '简装修';
+                break;
+            case 5:
+                return '毛坯';
+                break;
+            default;
+                break;
+        }
+    }
+
+    // 录入人
+    public function entryPerson()
+    {
+        return $this->belongsTo(User::class, 'guid', 'entry_person');
+    }
+    
+    // 维护人 
+    public function guardianPerson()
+    {
+        return $this->belongsTo(User::class, 'guid', 'guardian_person');
+    }
+
+    // 跟进
+    public function track()
+    {
+        return $this->hasMany(Track::class,'rel_guid', 'guid')->where('model_type', 'App\Models\Customer');
+    }
+
+    // 带看
+    public function remind()
+    {
+        return $this->hasMany(Visit::class,'cover_rel_guid', 'guid')->where('model_type', 'App\Models\Customer');
+    }
+
 }
---
+++ app/Services/CustomersService.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Services;
+
+use App\Handler\Common;
+use App\Models\Customer;
+
+class CustomersService
+{
+    //客源列表
+    public function getList($request)
+    {
+        return Customer::where('company_guid', Common::user()->company_guid)->paginate($request->per_page??10);
+    }
+
+    // 添加客源
+    public function addCustomer($request)
+    {
+        return Customer::create([
+            'guid' => Common::getUuid(),
+            'company_guid' => Common::user()->company_guid,
+            'level' => $request->level,
+            'guest' => $request->guest,
+            'customer_info' => $request->customer_info,
+            'remarks' => $request->remarks,
+            'intention' => $request->intention,
+            'block' => $request->block,
+            'building' => $request->building,
+            'house_type' => $request->house_type,
+            'price' => $request->price,
+            'acreage' => $request->acreage,
+            'type' => $request->type,
+            'renovation' => $request->renovation,
+            'floor' => $request->floor,
+            'target' => $request->target,
+            'status' => 1,
+            'entry_person' => Common::user()->guid,
+            'guardian_person' => Common::user()->guid,
+            'track_time' => date('Y-m-d H:i:s',time())  // 第一次跟进时间
+        ]);
+    }
+
+    // 更新客源
+    public function updateCustomer($customer, $request)
+    {
+        $customer->level = $request->level;
+        $customer->guest = $request->guest;
+        $customer->customer_info = $request->customer_info;
+        $customer->remarks = $request->remarks;
+        $customer->intention = $request->intention;
+        $customer->block = $request->block;
+        $customer->building = $request->building;
+        $customer->house_type = $request->house_type;
+        $customer->price = $request->price;
+        $customer->acreage = $request->acreage;
+        $customer->type = $request->type;
+        $customer->renovation = $request->renovation;
+        $customer->floor = $request->floor;
+        $customer->target = $request->targets;
+        if (!$customer->save()) return false;
+        return true;
+    }
+
+    public function getCustomerInfo($customer)
+    {
+
+    }
+
+    // 客源转为无效
+    public function invalid($guid, $request)
+    {
+        return Customer::where('guid', $guid)->update([
+            'status' => $request->status,
+            'reason' => $request->reason
+        ]);
+    }
+
+    // 更改客源类型(公私盘)
+    public function updateGuest($guid, $request)
+    {
+        return Customer::where('guid', $guid)->update(['guest' => $request->guest]);
+    }
+
+    // 转移客源
+    public function transfer($guid, $request)
+    {
+        return Customer::where('guid', $guid)->update(['guardian_person' => $request->guardian_person]);
+    }
+
+
+    // 获取正常状态的客源下拉数据
+    public function normalCustomer()
+    {
+        $res = Customer::where([
+            'company_guid' => Common::user()->company_guid,
+            'status' => 1
+        ])->get();
+        return $res->map(function ($v){
+            return [
+                'value' => $v->guid,
+                'label' => '客户:'.$v->customer_info[0]['name'].'  电话:'.$v->customer_info[0]['tel']
+            ];
+        });
+    }
+}

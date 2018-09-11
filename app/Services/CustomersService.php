5b097fbe1bbcce0f15d89584000a5306a1259881
luozhen <15902773450@163.com>
2018-09-11 10:32:22 +0800

    最新代码

--- app/Services/CustomersService.php
+++ app/Services/CustomersService.php
@@ -110,16 +110,17 @@ class CustomersService
 
         // 判断是否有修改维护人的权限
         $guardian_person = Access::adoptGuardianPersonGetCustomer('set_customer_guardian_person');
-        if (!in_array($guid, $guardian_person))  $permission['guardian_person'] = false;
+        if (!in_array($guid, $guardian_person)) $permission['guardian_person'] = false;
 
         // 是否允许带看
         $seeCustomerVisit = Access::adoptGuardianPersonGetCustomer('see_customer_visit');
-        if (!in_array($guid, $seeCustomerVisit)) $data['visit_permission'] = false;
+        if (!in_array($guid, $seeCustomerVisit)) $permission['visit_permission'] = false;
 
         // 是否允许编辑客源
         $editCustomer = Access::adoptGuardianPersonGetCustomer('edit_customer');
         if (!in_array($guid,$editCustomer)) $permission['edit_customer'] = false;
-       $data = [];
+
+        $data = [];
         $data['permission'] = $permission;
         $data['guid'] = $res->guid;
         $data['level'] = $res->level_cn;

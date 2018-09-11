b1169a38bcef879d4a03aec0a4ba217bc9b566c2
刘坤涛 <287667404@qq.com>
2018-09-11 10:39:44 +0000

    修改

--- app/Services/SharesService.php
+++ app/Services/SharesService.php
@@ -49,11 +49,11 @@ class SharesService
         $data['indoor_img_url'] = $house->indoor_img_url??[]; // 室内图
         $data['house_type_img_url'] = $house->house_type_img_url??[]; // 户型图
         $data['outdoor_img_url'] = $house->outdoor_img_url??[]; // 室外图
-        $data['buildingName'] = $house->buildingBlock->building->name??'-'; // 楼盘名
-        $data['grade'] = $house->grade_cn??'-'; // 级别
+        $data['buildingName'] = $house->buildingBlock->building->name??'暂无'; // 楼盘名
+        $data['grade'] = $house->grade_cn??'暂无'; // 级别
         $data['price_unit'] = $house->price . $house->price_unit_cn; //价格单位
-        $data['payment_type'] = $house->payment_type_cn??'-'; //付款方式
-        $data['acreage'] = $house->acreage_cn??'-'; //面积
+        $data['payment_type'] = $house->payment_type_cn??'暂无'; //付款方式
+        $data['acreage'] = $house->acreage_cn??'暂无'; //面积
         // 楼层
         if ($house->buildingBlock->total_floor) {
             $data['floor'] = $house->floor.'/'.$house->buildingBlock->total_floor;
@@ -66,20 +66,19 @@ class SharesService
         // 费用明细
         $data['cost_detail'] = empty($house->cost_detail)?'暂无':implode(',', $house->cost_detail);
         $data['source'] = $house->source_cn; // 来源渠道
-        $data['increasing_situation_remark'] = $house->increasing_situation_remark??'-'; // 递增情况
+        $data['increasing_situation_remark'] = $house->increasing_situation_remark??'暂无'; // 递增情况
         $data['split'] = $house->split_cn; // 拆分
         $data['mini_acreage'] = empty($house->mini_acreage)?'暂无':$house->mini_acreage.'㎡'; // 最小面积
         $data['floor_height'] = empty($house->floor_height)?'暂无':$house->floor_height.'m'; // 层高
-        $data['property_fee'] = $house->buildingBlock->property_fee_cn??'-'; // 物业费
-        $data['register_company'] = $house->register_company_cn??'-'; // 是否注册
-        $data['open_bill'] = $house->open_bill_cn??'-'; // 可开发票
+        $data['property_fee'] = $house->buildingBlock->property_fee_cn??'暂无'; // 物业费
+        $data['register_company'] = $house->register_company_cn??'暂无'; // 是否注册
+        $data['open_bill'] = $house->open_bill_cn??'暂无'; // 可开发票
         // 工位数量
         $data['station_number'] = empty($house->station_number)?'暂无':$house->station_number.'个';
         $data['rent_free'] = empty($house->rent_free)?'暂无':$house->rent_free.'天'; // 免租期
         $data['shortest_lease'] = $house->shortest_lease_cn??'暂无'; // 最短租期
         $data['support_facilities'] = empty($house->support_facilities)?'暂无':implode(',',$house->support_facilities); // 配套设施
-        $houses['belong'] = $house->release_source == '平台' ? '平台' : $house->company_guid == Common::user()->company_guid ? '本公司' : '其他公司'; // 房源归属
-
+        $data['actuality'] = $house->actuality_cn;
         //联系方式
         $contact = [];
         if ($house->release_source == '平台') {
@@ -138,36 +137,36 @@ class SharesService
         $data['indoor_img_url'] = $house->indoor_img_url??[]; // 室内图
         $data['house_type_img_url'] = $house->house_type_img_url??[]; // 户型图
         $data['outdoor_img_url'] = $house->outdoor_img_url??[]; // 室外图
-        $data['buildingName'] = $house->buildingBlock->building->name??'-'; // 楼盘名
+        $data['buildingName'] = $house->buildingBlock->building->name??'暂无'; // 楼盘名
         // 门牌号
         if (empty($house->buildingBlock->unit)) {
             $data['house_number'] = $house->buildingBlock->name.$house->buildingBlock->name_unit.' '.$house->house_number.' '.$house->house_number;
         } else {
             $data['house_number'] = $house->buildingBlock->name.$house->buildingBlock->name_unit.' '.$house->buildingBlock->unit.$house->buildingBlock->unit_unit.' '.$house->house_number;
         }
-        $data['grade'] = $house->grade_cn??'-'; // 级别
+        $data['grade'] = $house->grade_cn??'暂无'; // 级别
         $data['price_unit'] = $house->price . $house->price_unit_cn; //价格单位
-        $data['payment_type'] = $house->payment_type_cn??'-'; //付款方式
-        $data['acreage'] = $house->acreage_cn??'-'; //面积
+        $data['payment_type'] = $house->payment_type_cn??'暂无'; //付款方式
+        $data['acreage'] = $house->acreage_cn??'暂无'; //面积
         // 楼层
         if ($house->buildingBlock->total_floor) {
             $data['floor'] = $house->floor.'/'.$house->buildingBlock->total_floor;
         } else {
             $data['floor'] = $house->floor;
         }
-        $data['orientation'] = $house->orientation_cn??'-'; //朝向
-        $data['renovation'] = $house->renovation_cn??'-';  //装修程度
-        $data['type'] = $house->type_cn??'-'; //类型
+        $data['orientation'] = $house->orientation_cn??'暂无'; //朝向
+        $data['renovation'] = $house->renovation_cn??'暂无';  //装修程度
+        $data['type'] = $house->type_cn??'暂无'; //类型
         // 费用明细
         $data['cost_detail'] = empty($house->cost_detail)?'暂无':implode(',', $house->cost_detail);
         $data['source'] = $house->source_cn; // 来源渠道
-        $data['increasing_situation_remark'] = $house->increasing_situation_remark; // 递增情况
+        $data['increasing_situation_remark'] = $house->increasing_situation_remark??'-'; // 递增情况
         $data['split'] = $house->split_cn; // 拆分
         $data['mini_acreage'] = empty($house->mini_acreage)?'暂无':$house->mini_acreage.'㎡'; // 最小面积
         $data['floor_height'] = empty($house->floor_height)?'暂无':$house->floor_height.'m'; // 层高
         $data['property_fee'] = $house->buildingBlock->property_fee_cn??'-'; // 物业费
-        $data['register_company'] = $house->register_company_cn??'-'; // 是否注册
-        $data['open_bill'] = $house->open_bill_cn??'-'; // 可开发票
+        $data['register_company'] = $house->register_company_cn??'暂无'; // 是否注册
+        $data['open_bill'] = $house->open_bill_cn??'暂无'; // 可开发票
         // 工位数量
         $data['station_number'] = empty($house->station_number)?'暂无':$house->station_number.'个';
         $data['rent_free'] = empty($house->rent_free)?'暂无':$house->rent_free.'天'; // 免租期

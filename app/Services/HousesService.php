fbdf7e2c43f19f721eee39bac771375a83f6d487
luozhen <15902773450@163.com>
2018-09-20 08:53:00 +0800

    范围筛选修改

--- app/Services/HousesService.php
+++ app/Services/HousesService.php
@@ -893,7 +893,7 @@ class HousesService
         if ($request->range) {
 
             if ($request->range == 4) {
-                $guardian_person = Common::user()->guid;
+                $guardian_person[] = Common::user()->guid;
             } else {
                 $guardian_person = $this->getCompanyRange($request->range);
             }

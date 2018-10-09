bb499568cce67cf8bbf26c78d46b68ca1979df40
luozhen <15902773450@163.com>
2018-10-09 18:35:08 +0800

    修改配置

--- config/database.php
+++ config/database.php
@@ -73,7 +73,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE3', 'forge'),
+            'database' => env('DB_DATABASE4', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
